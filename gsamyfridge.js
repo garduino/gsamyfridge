a85356ecdd58dcee2c3174d3c0234ca34d09e4d2
garduino <gsa@arsol.net>
2015-10-08 14:27:54 -0300

    Intermediate Saving.

--- gsamyfridge.js
+++ gsamyfridge.js
@@ -1,25 +1,42 @@
 Products = new Mongo.Collection('products');
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.fridge.helpers({
+    products: function () {
+      return Products.find({
+        place: 'fridge'
+      });
+    }
+  });
+  Template.productList.helpers({
+    products: function () {
+      return Products.find({
+        place: 'supermarket'
+      });
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+  }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
+    // Empty the database
+    Products.remove({});
+
+    // fill the database with some products
+    Products.insert({
+      name: 'Milk',
+      img: '/milk.png',
+      place: 'fridge'
+      });
+
+    Products.insert({
+      name: 'Bread',
+      img: '/bread.png',
+      place: 'supermarket'
+      });
+    });
+
 }
