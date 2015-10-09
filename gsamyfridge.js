c880ecae816d38965ca207ce10c8ec0af0768a84
garduino <gsa@arsol.net>
2015-10-09 10:47:20 -0300

    Added more products in the fridge and supermarket.

--- gsamyfridge.js
+++ gsamyfridge.js
@@ -72,6 +72,17 @@ if (Meteor.isServer) {
       img: '/bread.png',
       place: 'supermarket'
       });
-    });
 
+    Products.insert({
+      name: 'Banana',
+      img: '/banana.png',
+      place: 'supermarket'
+      });
+
+    Products.insert({
+      name: 'Juice',
+      img: '/juice.png',
+      place: 'fridge'
+      });
+    });
 }
