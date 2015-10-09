0cbf5788d9079d45717a98950cd1bd88f1817e21
garduino <gsa@arsol.net>
2015-10-09 10:04:17 -0300

    Chapter 2 completed.

--- gsamyfridge.js
+++ gsamyfridge.js
@@ -9,6 +9,7 @@ if (Meteor.isClient) {
       });
     }
   });
+
   Template.productList.helpers({
     products: function () {
       return Products.find({
@@ -16,6 +17,40 @@ if (Meteor.isClient) {
       });
     }
   });
+
+  Template.fridge.onRendered(function () {
+    var templateInstance = this;
+    templateInstance.$('#fridge').droppable({
+      drop: function(evt, ui) {
+        var query = { _id: ui.draggable.data('id') };
+        var changes = { $set: { place: 'fridge' } };
+        Products.update(query, changes);
+      }
+      });
+    });
+
+
+  Template.productList.onRendered(function() {
+    var templateInstance = this;
+      templateInstance.$('#supermarket').droppable({
+        drop: function(evt, ui) {
+          var query = { _id: ui.draggable.data('id') };
+          var changes = { $set: { place: 'supermarket' } };
+          Products.update(query, changes);
+        }
+      });
+    });
+
+
+
+  Template.productListItem.onRendered(function() {
+    var templateInstance = this;
+    templateInstance.$('.draggable').draggable({
+      cursor: 'move',
+      helper: 'clone'
+    });
+  });
+
 
   }
 
